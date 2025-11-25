648b02ee0fab466209d255f5462282514802d4e7
Flamarionfp <flamarionpinto@sou.faccat.br>
2025-11-25 14:15:01 -0300

    feat: splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tribus/screens/splash.screen.dart';
 import 'package:tribus/screens/register_recyclable_material.screen.dart';
 import 'package:tribus/theme/app_theme.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load();
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const AppInitializer());
+}
+
+class AppInitializer extends StatefulWidget {
+  const AppInitializer({super.key});
+
+  @override
+  State<AppInitializer> createState() => _AppInitializerState();
+}
+
+class _AppInitializerState extends State<AppInitializer> {
+  bool loaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Future<void> init() async {
+    await dotenv.load();
+    await Future.delayed(const Duration(milliseconds: 400));
+
+    setState(() => loaded = true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!loaded) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        home: const SplashScreen(),
+      );
+    }
+
+    return const MyApp();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -16,8 +55,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tribus',
       theme: AppTheme.lightTheme,
-      home: const RegisterRecyclableMaterialScreen(),
       debugShowCheckedModeBanner: false,
+      home: const RegisterRecyclableMaterialScreen(),
     );
   }
 }
---
+++ lib/screens/splash.screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/recycle.png', width: 90),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
